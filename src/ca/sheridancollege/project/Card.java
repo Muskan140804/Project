01784fea13c10178b83b11ba583b3a8d448b7ff9
jaspuneetkaurkalkat11 <kalkjasp@sheridancollege.ca>
2024-08-08 13:25:23 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -11,15 +11,91 @@ package ca.sheridancollege.project;
  *
  * @author dancye
  */
-public abstract class Card {
-    //default modifier for child classes
+public class Card
+{
+    public string Suit { get; set; }
+    public string Rank { get; set; }
 
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    @Override
-    public abstract String toString();
+    public Card(string suit, string rank)
+    {
+        Suit = suit;
+        Rank = rank;
+    }
+
+    public override string ToString()
+    {
+        return $"{Rank} of {Suit}";
+    }
+}
+
+public class Deck
+{
+    private List<Card> cards;
+
+    public Deck()
+    {
+        cards = new List<Card>();
+        string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+        string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
+        foreach (var suit in suits)
+        {
+            foreach (var rank in ranks)
+            {
+                cards.Add(new Card(suit, rank));
+            }
+        }
+
+        Shuffle();
+    }
+
+    public void Shuffle()
+    {
+        Random rand = new Random();
+        cards = cards.OrderBy(card => rand.Next()).ToList();
+    }
+
+    public Card DrawCard()
+    {
+        if (cards.Count > 0)
+        {
+            var card = cards[0];
+            cards.RemoveAt(0);
+            return card;
+        }
+        return null;
+    }
+
+    public int CardsRemaining()
+    {
+        return cards.Count;
+    }
+}
+
+public class Player
+{
+    public string Name { get; set; }
+    public List<Card> Hand { get; private set; }
+
+    public Player(string name)
+    {
+        Name = name;
+        Hand = new List<Card>();
+    }
+
+    public void DrawCard(Deck deck)
+    {
+        var card = deck.DrawCard();
+        if (card != null)
+        {
+            Hand.Add(card);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} has {Hand.Count} cards.";
+    }
+}
 
 }
