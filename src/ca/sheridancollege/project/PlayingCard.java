5c6cba7d57a5704f739c4b9045a4d85e367780bf
jaspuneetkaurkalkat11 <kalkjasp@sheridancollege.ca>
2024-08-11 16:21:01 -0400

    Update PlayingCard.java

--- src/ca/sheridancollege/project/PlayingCard.java
+++ src/ca/sheridancollege/project/PlayingCard.java
@@ -1,1 +1,25 @@
+package ca.sheridancollege.project;
 
+public class PlayingCard extends Card {
+
+    private Rank rank;
+    private Suit suit;
+
+    public PlayingCard(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
